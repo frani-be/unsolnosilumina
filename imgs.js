f0fe7e32b59fa81484a5610f0d4401142050dbb4
Francisca Medina Concha <fbmedinaconcha@gmail.com>
2024-01-26 22:17:14 -0300

    script with DocumentFragment to optimize load of images

--- imgs.js
+++ imgs.js
@@ -1,1 +1,46 @@
-const gallery=document.getElementById("gallery"),images=[];let imageData=[];function csvToArray(a){const e=a.trim().split("\n"),t=e[0].split(",");return e.slice(1).map((a=>{const e=a.split(","),n={};return t.forEach(((a,t)=>{n[a]=e[t]})),n}))}function shuffleArray(a){for(let e=a.length-1;e>0;e--){const t=Math.floor(Math.random()*(e+1));[a[e],a[t]]=[a[t],a[e]]}}fetch("imgs.csv").then((a=>a.text())).then((a=>{imageData=csvToArray(a);const e=imageData.length;for(let a=0;a<e;a++){const e=document.createElement("img");e.loading="lazy",e.alt=imageData[a].descripcion||"Imagen de Un Sol Nos Ilumina by frani.be (Francisca Beatriz Medina Concha)",e.title=imageData[a].descripcion||"Imagen de Un Sol Nos Ilumina by frani.be (Francisca Beatriz Medina Concha)",e.src=`imgs/${imageData[a].imagen}`;const t=document.createElement("figure");t.appendChild(e),images.push(t)}shuffleArray(images),images.forEach((a=>gallery.appendChild(a)))}));
+const gallery = document.getElementById("gallery");
+let images = [];
+let imageData = [];
+
+function csvToArray(str) {
+    const rows = str.trim().split("\n");
+    const headers = rows[0].split(",");
+    return rows.slice(1).map(row => {
+        const values = row.split(",");
+        const el = {};
+        headers.forEach((header, index) => {
+            el[header] = values[index];
+        });
+        return el;
+    });
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+fetch("imgs.csv")
+    .then(response => response.text())
+    .then(data => {
+        imageData = csvToArray(data);
+        const fragment = document.createDocumentFragment();
+
+        imageData.forEach(data => {
+            const img = document.createElement("img");
+            img.loading = "lazy";
+            img.alt = data.descripcion || "Imagen de Un Sol Nos Ilumina by frani.be (Francisca Beatriz Medina Concha)";
+            img.title = data.descripcion || "Imagen de Un Sol Nos Ilumina by frani.be (Francisca Beatriz Medina Concha)";
+            img.src = `imgs/${data.imagen}`;
+
+            const figure = document.createElement("figure");
+            figure.appendChild(img);
+            images.push(figure);
+        });
+
+        shuffleArray(images);
+        images.forEach(image => fragment.appendChild(image));
+        gallery.appendChild(fragment);
+    });
